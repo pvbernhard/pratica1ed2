cacd19e3d93c300bceb622bcba7a12050c7d1ace
Pedro V. Bernhard <pvbernhard@gmail.com>
2021-07-18 17:12:21 -0300

    testa MergeSort

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import dataset.Dado;
 import dataset.Dados;
+import ordenamento.MergeSort;
 
 import java.util.ArrayList;
 
@@ -9,10 +10,17 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Dado> dados = new ArrayList<>();
         try {
-            dados = Dados.gerarDados("a", "crescente", 1000);
+            dados = Dados.gerarDados("c", "decrescente", 10);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        for (Dado dado : dados) {
+            System.out.println(dado);
+        }
+
+        MergeSort.sort(dados);
+
+        System.out.println("Ordenado:");
         for (Dado dado : dados) {
             System.out.println(dado);
         }
