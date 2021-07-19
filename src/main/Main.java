c2821a2918b754b53fd5d7b605ba2a2fb755b717
Pedro V. Bernhard <pvbernhard@gmail.com>
2021-07-18 22:59:36 -0300

    relatorios

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,16 @@
 package main;
 
-import ordenamento.MergeSort;
+import ordenamento.*;
 import relatorio.Relatorio;
 
 public class Main {
     public static void main(String[] args) {
         Relatorio.geraRelatorio(
-                new int[]{10, 100, 1000, 10000},
+                new int[]{10, 100, 1000, 10000, 100000},
+                50,
                 new String[]{"aleatorio"},
                 new String[]{"a", "b", "c"},
-                new MergeSort(),
+                new QuickSortComInsertionSort(15),
                 false);
     }
 }
